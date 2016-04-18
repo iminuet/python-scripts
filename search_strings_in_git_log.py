6d42797f4c028aad65939b0483413c6b925acb9e
Gong Qianyu <Qianyu.Gong@nxp.com>
2016-04-18 19:11:48 +0800

    search_strings_in_git_log.py: add function to get commit ID from 'git log'
    Signed-off-by: Gong Qianyu <Qianyu.Gong@nxp.com>

--- search_strings_in_git_log.py
+++ search_strings_in_git_log.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import os
+import os,re
 #File: search_strings_in_git_log.py
 #Goal: Use this script to find specified strings in git log such as
 #	" fman ", " nand " and kinds of IP blocks. Then it shows
@@ -17,17 +17,30 @@ patch_content = []
 
 string_name = raw_input("Enter the IP block name: ")
 
-for each_new_file in new_patchset:
-	try:
-		loginfo = os.popen("git log -p").readlines()
-		if loginfo.find(" " + string_name + " ")
-				
-		count = 1
-		new_flag = 0
-		message_line_num = 0
-		print("===============================================================")
-		
-		f.close()
-	except:
-		#pass
-		print("Error!")
+def get_commit_in_log_line(line):	#deal with one line at a time
+	if line[:7] == "commit ":
+		line = re.findall(r'(.{7})', line) #return every 6 chars
+		print(line[1] + "\n")
+
+def get_commit_in_patch_line(in_file):
+	line = in_file.readline()     #just read the first line
+	my_line = re.findall(r'(.{4})', line) #return every 4 chars
+	if my_line[0] == "From":
+		#Commit ID should be at least 6 chars
+		f.write(my_line[1] + my_line[2]+ "\n")
+	return
+
+#for each_new_file in new_patchset:
+try:
+	loginfo = os.popen("git log").readlines()
+	for i in range(0, 10):
+		get_commit_in_log_line(loginfo[i])
+
+	count = 1
+	new_flag = 0
+	message_line_num = 0
+	print("===============================================================")
+
+except:
+	#pass
+	print("Error!")
