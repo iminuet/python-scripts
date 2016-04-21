4f492715a11bc28a88cc196e83e4fb56f525d25f
Gong Qianyu <Qianyu.Gong@nxp.com>
2016-04-21 16:23:57 +0800

    search_strings...py: Add mark_ip_in_stat() function
    mark_ip_in_stat: could mark the user's key string in commit_patch.txt file.
    
    The updated format of "commit_patch.txt" will be like this:
    "
    10014 7a9b740 QSPI
    10015 4c4fc64 QSPI,NOR
    10016 638c7e6 NAND,NOR
    ..."
    
    Signed-off-by: Gong Qianyu <Qianyu.Gong@nxp.com>

--- search_strings_in_git_log.py
+++ search_strings_in_git_log.py
@@ -16,9 +16,13 @@ import os, re, time
 #keep track of the line numbers of current commit and next commit 
 cur_commit_line = 0
 next_commit_line = 0
+#Need the generated file as the input:
+commit_file = open("./commit_patch.txt",\
+"r").readlines()
 #write the result to a new file named by the date:
 ISOTIMEFORMAT='%Y-%m-%d-%H-%M-%S'
-statistics_file = open(str(time.strftime(ISOTIMEFORMAT)), "w+")
+stat_file = open(str(time.strftime(ISOTIMEFORMAT)), "w+")
+write_count = 0
 
 def get_commit_in_log_line(line):    #deal with one line at a time
     if line[:7] == "commit ":
@@ -36,18 +40,44 @@ def get_commit_in_patch_line(in_file):
 def search_strings_in_log(log, key):#(string0,*argv):
     for i in range(len(log) - 1, 0, -1):#!!#search from the bottom of the log
         if log[i].find(key) > 0: #!!#if no that key,return -1;##########
-	    print(log[i])
+            print(log[i])
             for j in range(0, 20):#20*2 is an assumed length of one log
                 if log[i - j][:7] == "commit ":
                     cur_commit_line =  i - j
                     cur_commit_id = get_commit_in_log_line(log[cur_commit_line])
                     i = cur_commit_line
-                    statistics_file.write(cur_commit_id + "|" + key +"|\n")
-		    break
-	elif i == 0:
+                    #print(cur_commit_id)
+                    mark_ip_in_stat(cur_commit_id, key)
+                    break
+        elif i == 1:
             print(" Search to TOP of the file!")
     return 
 
+def mark_ip_in_stat(commit_id, ip_key):
+    global write_count
+    for i in range(write_count, len(commit_file) + 1):
+        print(str(i))
+        each_line_list = commit_file[i].split(' ')
+        #print(each_line_list)
+        ##['10021', '764c05a\n'] or
+        ##['10021', '764c05a', 'QSPI\n'] or
+        ##['10021', '764c05a', 'QSPI,IFC\n']
+        if each_line_list[1][:7] == commit_id:
+            if len(each_line_list) < 3:
+                commit_file[i] = commit_file[i][:-1] + " " +\
+		ip_key + "\n"
+            #elif each_line_list[2].find(',') == -1:
+            else:
+                commit_file[i] = commit_file[i][:-1] + "," +\
+		ip_key + "\n"
+            #debug
+            #commit_file[i] = ("").join(each_line_list)
+            #stat_file.write(commit_file[i])
+            print(commit_file[i])
+
+	    write_count = i + 1
+	    break
+    return
 #for each_new_file in new_patchset:
 try:
     string_name = raw_input("Enter the IP block name: ")
@@ -56,6 +86,7 @@ try:
     lognum = 50
     loginfo = os.popen("git log -" + str(lognum)).readlines()
     search_strings_in_log(loginfo, string_name)
+    stat_file.writelines(commit_file)
 
 
     print("==========================================================")
