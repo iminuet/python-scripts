394448ce4598b995e0f02712c841a5a6322abdba
Gong Qianyu <Qianyu.Gong@nxp.com>
2017-03-01 11:59:58 +0800

    byte_swap.py: revised the code
    Signed-off-by: Gong Qianyu <Qianyu.Gong@nxp.com>

--- byte_swap.py
+++ byte_swap.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python
 """
-Swap the bytes endian except for PBI CRC
-2016-10-9: Add this script
+Swap the 4/8 bytes endian except for PBI CRC
+2016-10-9: Initial version
 
 Usage:
-    ./byte_swap.py <file_name> <byte>
+	./byte_swap.py <file_name> <byte>
 """
 import sys
 
 try:
-    file_name = sys.argv[1]
-    byte = int(sys.argv[2])
+	file_name = sys.argv[1]
+	byte = int(sys.argv[2])
 except:
-    print("Usage: ./byte_swap.py <file_name> <byte>")
-    print("eg: ./byte_swap.py rcw_1600.bin 8\n")
-    exit
+	print("Usage: ./byte_swap.py <file_name> <byte>")
+	print("E.g.: ./byte_swap.py rcw_1600.bin 8\n")
+	exit
 
 with open(file_name,'rb') as file:
-    tmp = file.read()
+	tmp = file.read()
 file.close()
 
 with open(file_name + '.swap','wb') as file:
-    for i in range(0, len(tmp) - 1, byte):
-    	if(tmp[i:i+4].encode('hex')) == "08610040":
-    		#print("PBI CRC command")
-    		file.write(tmp[i:i+8])
-    		break
-	file.write(tmp[i:i+byte][::-1])
-print("Swapped file: " + file_name + '.swap')
+	for i in range(0, len(tmp) - 1, byte):
+		if(tmp[i:i+4].encode('hex')) == "08610040":
+			#print("PBI CRC command")
+			file.write(tmp[i:i+8])
+			break
+		file.write(tmp[i:i+byte][::-1])
 file.close()
+print("Swapped file: " + file_name + '.swap')
